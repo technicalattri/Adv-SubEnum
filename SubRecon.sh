09f0fcb664fde164745953e215d6530750845fe2
Nitin Attri <113044247+technicalattri@users.noreply.github.com>
2024-09-02 03:59:33 +0530

    Update SubRecon.sh

--- SubRecon.sh
+++ SubRecon.sh
@@ -10,14 +10,14 @@ CP='\033[1;38;5;221m'
 
 function banner(){
     echo -e ${RED}"##################################################################"
-    echo -e ${CP}"        ____  _            _      ____  _____           _   _    #"
-    echo -e ${CP}"       | __ )| | __ _  ___| | __ |  _ \|___ /  ___ ___ | \ | |   #"
-    echo -e ${CP}"       |  _ \| |/ _  |/ __| |/ / | |_) | |_ \ / __/ _ \|  \| |   #"
-    echo -e ${CP}"       | |_) | | (_| | (__|   <  |  _ < ___) | (_| (_) | |\  |   #"
-    echo -e ${CP}"       |____/|_|\__ _|\___|_|\_\ |_| \_\____/ \___\___/|_| |_|   #"
-    echo -e ${CP}"              Subdomain Enumeration Tool                         #"
-    echo -e ${BLUE}"              https://github.com/technicalattri                #"
-    echo -e ${YELLOW}"              Coded By: Nitin Attri && Komal0x01             #"
+    echo -e ${CP}"         ____        _     ____                                   #"
+    echo -e ${CP}"        / ___| _   _| |__ |  _ \ ___  ___ ___  _ __               #"
+    echo -e ${CP}"        \___ \| | | | '_ \| |_) / _ \/ __/ _ \| '_ \              #"
+    echo -e ${CP}"         ___) | |_| | |_) |  _ <  __/ (_| (_) | | | |             #"
+    echo -e ${CP}"        |____/ \__,_|_.__/|_| \_\___|\___\___/|_| |_|             #"
+    echo -e ${CP}"              Subdomain Enumeration Tool                          #"
+    echo -e ${BLUE}"              https://github.com/technicalattri                 #"
+    echo -e ${YELLOW}"              Coded By: Nitin Attri && Komal0x01              #"
     echo -e ${RED}"################################################################## \n "
 }
 
